9592972a2b4ca42db211dbb3c4ffc7499eb37a36
Aaditya Bharadwaj <me.purezen@gmail.com>
2025-11-23 01:48:43 +0530

    ui: improve Home Screen styling

--- composeApp/src/commonMain/kotlin/com/example/audiodescriptor/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/audiodescriptor/App.kt
@@ -1,13 +1,18 @@
 package com.example.audiodescriptor
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.Navigator
@@ -41,7 +46,10 @@ class HomeScreen() : Screen {
         ) {
             Text("AudioDescriptor", style = MaterialTheme.typography.headlineLarge)
 
-            Text("Let's start with a sample task for practice")
+            Text("Let's start with a sample task for practice",
+                modifier = Modifier.padding(20.dp),
+                style = MaterialTheme.typography.headlineMedium,
+            )
 
             Text("Pehele hum ek sample task karte hain")
 
